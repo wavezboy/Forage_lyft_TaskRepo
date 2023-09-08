19125bce84b6d22b454b96facd3160ce46a6861a
ABDULFATTAH OLADAYO <sanusifatah16@gmail.com>
2023-09-08 14:47:34 +0100

    done

--- car.py
+++ car.py
@@ -1,14 +1,14 @@
 from abc import ABC, abstractmethod
 from engine.engine import Engine
 from battery.battery import Battery
+from serviceable import Serviceable
 
 
-class Car(Engine, Battery):
+class Car(Serviceable):
     def __init__(self):
         self.engine = Engine
         self.battery = Battery
         
-    
 
     @abstractmethod
     def needs_service(self):
